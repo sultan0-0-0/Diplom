73a566615e0e39cfce2d840c19236ceeb55cfba2
Ahmed Mahmoud <92227588+sultan0-0-0@users.noreply.github.com>
2024-05-23 00:27:32 +0300

    Use the hyerogliphics dataset

--- Diplom.py
+++ Diplom.py
@@ -4,7 +4,67 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dens
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Загрузка и подготовка данных
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+train_data = pd.read_csv("/kaggle/input/egyptian-hieroglyphs/train/_annotations.csv")
+train_data.info()
+valid_data = pd.read_csv("/kaggle/input/egyptian-hieroglyphs/valid/_annotations.csv")
+
+test_data = pd.read_csv("/kaggle/input/egyptian-hieroglyphs/test/_annotations.csv")
+
+import os
+import cv2
+
+train_folder = "/kaggle/input/egyptian-hieroglyphs/train/"
+valid_folder = "/kaggle/input/egyptian-hieroglyphs/valid/"
+test_folder = "/kaggle/input/egyptian-hieroglyphs/test/"
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        if filename.endswith('.jpg'):
+            img = cv2.imread(os.path.join(folder, filename))
+            if img is not None:
+                images.append(img)
+            else:
+                continue
+    return images
+
+
+train_images = load_images_from_folder(train_folder)
+
+valid_images = load_images_from_folder(valid_folder)
+
+test_images = load_images_from_folder(test_folder)
+
+train_labels = train_data['class'].tolist()
+valid_labels = valid_data['class'].tolist()
+test_labels = test_data['class'].tolist()
+
+train_images = np.array(train_images)
+valid_images = np.array(valid_images)
+test_images = np.array(test_images)
+train_labels = np.array(train_labels)
+valid_labels = np.array(valid_labels)
+test_labels = np.array(test_labels)
+
+from sklearn.preprocessing import OrdinalEncoder
+
+encoder = OrdinalEncoder()
+train_labels = encoder.fit_transform([train_labels])
+valid_labels = encoder.fit_transform([valid_labels])
+test_labels = encoder.fit_transform([test_labels])
+train_labels = train_labels.flatten()
+valid_labels = valid_labels.flatten()
+test_labels = test_labels.flatten()
+import gc
+del(train_data)
+del(valid_data)
+del(test_data)
+
+gc.collect()
+
+
+
+
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
